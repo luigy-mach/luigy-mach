d52b68d19a1c70fc2080569650d741f0583bd697
luigy-mach <luigy-mach>
2019-03-25 13:53:43 -0300

    Updates

---
+++ precache-manifest.4ffb87df58a441bb67974514b6d4639f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04e9b89869d5f1201272",
+    "url": "/luigy-mach/static/css/main.0f92fe87.chunk.css"
+  },
+  {
+    "revision": "04e9b89869d5f1201272",
+    "url": "/luigy-mach/static/js/main.f69e02dd.chunk.js"
+  },
+  {
+    "revision": "00b32429aa6b84265b36",
+    "url": "/luigy-mach/static/js/runtime~main.bdd57a0c.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/luigy-mach/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/luigy-mach/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4cf8a28ae4bac0e059ad83d00a675379",
+    "url": "/luigy-mach/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de36b0c5b4e13bc8819a3e1285b63e6f.js"
+  "/luigy-mach/precache-manifest.4ffb87df58a441bb67974514b6d4639f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/luigy-mach/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
